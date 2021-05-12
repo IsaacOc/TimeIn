9454e092722dd2935c53a16ae42ee527d46e0204
isaa <isaa@gmail.com>
2021-05-12 19:30:20 +0200

    User modal

---
+++ app/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use App\Notifications\clockingIn;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    //sends nontification clockingIn
+    public function sendsNotificationsclockingIn()
+    {
+       
+    return $this->notify(new clockingIn());
+    
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','role',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+}
