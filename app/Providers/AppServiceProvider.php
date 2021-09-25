408af1be290f12d5407e5b2611fab28e55b7f846
isaa <isaa@gmail.com>
2021-09-25 14:55:47 +0200

    added Notification class in provider registry

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind(
+            'App\Repositories\PostRepositoryInterface',
+            'App\Repositories\PostRepository'
+        );
     }
 }
