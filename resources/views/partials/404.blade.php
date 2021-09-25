8ae9f6e597f301f78094a4b12a0c1f286d1c53a0
isaa <isaa@gmail.com>
2021-09-25 14:32:58 +0200

    error file for redirect

---
+++ resources/views/partials/404.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" style="margin-top:1em">
+    <div class="row " >
+        <div class="col-md-8 col-md-offset-2">
+            <div class="">
+                <div class="panel-heading" ><h4><b>NOT ALLOWED</b></h4></div>
+
+                <div class="panel-body">
+                <p>ALREADY REGISTERED YOUR TIME</P>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
